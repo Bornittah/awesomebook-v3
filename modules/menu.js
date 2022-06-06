43e7fa87fa02de679effea04387c4d1bb513ffa9
bornittah <bornittah@gmail.com>
2022-06-06 12:50:55 +0300

    fix linters errors

--- modules/menu.js
+++ modules/menu.js
@@ -1,33 +1,33 @@
 function navigationMenu() {
-const listSection = document.querySelector('.booklist');
-const addSection = document.querySelector('.addnew');
-const contactSection = document.querySelector('.contacts');
+  const listSection = document.querySelector('.booklist');
+  const addSection = document.querySelector('.addnew');
+  const contactSection = document.querySelector('.contacts');
 
-const listMenuLink = document.querySelector('#bookslist');
-const addMenuLink = document.querySelector('#addbooks');
-const contactMenuLink = document.querySelector('#contact');
+  const listMenuLink = document.querySelector('#bookslist');
+  const addMenuLink = document.querySelector('#addbooks');
+  const contactMenuLink = document.querySelector('#contact');
 
-document.querySelectorAll('.navbar').forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    listMenuLink.addEventListener('click', () => {
-      listSection.style.display = 'block';
-      addSection.style.display = 'none';
-      contactSection.style.display = 'none';
-    });
+  document.querySelectorAll('.navbar').forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      listMenuLink.addEventListener('click', () => {
+        listSection.style.display = 'block';
+        addSection.style.display = 'none';
+        contactSection.style.display = 'none';
+      });
 
-    addMenuLink.addEventListener('click', () => {
-      listSection.style.display = 'none';
-      addSection.style.display = 'block';
-      contactSection.style.display = 'none';
-    });
+      addMenuLink.addEventListener('click', () => {
+        listSection.style.display = 'none';
+        addSection.style.display = 'block';
+        contactSection.style.display = 'none';
+      });
 
-    contactMenuLink.addEventListener('click', () => {
-      listSection.style.display = 'none';
-      addSection.style.display = 'none';
-      contactSection.style.display = 'block';
+      contactMenuLink.addEventListener('click', () => {
+        listSection.style.display = 'none';
+        addSection.style.display = 'none';
+        contactSection.style.display = 'block';
+      });
     });
   });
-});
 }
 export { navigationMenu };
